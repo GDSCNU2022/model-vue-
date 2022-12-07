9cdabbb665b61399904141557adc8b0e2e7c7a4c
yamarico <81962317+yamarico@users.noreply.github.com>
2022-12-07 20:18:16 +0900

    tab

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,23 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/"> Nuxt Dojo </NuxtLink>
-                <ul>
-                    <li><NuxtLink to="/" >Home</NuxtLink></li>
-                    <li><NuxtLink to="/about" >About</NuxtLink></li>
-                    <li><NuxtLink to="/products" >products</NuxtLink></li>
-                </ul>
-                <a href="/about"> normal link -- about page </a>
-            </nav>
-        </header>
-        <div>
-            <slot />
-        </div>
+      <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4 flex justify-between">
+          <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+          <ul class="flex gap-4">
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/products" class="btn">Merch</NuxtLink></li>
+          </ul>
+        </nav>
+      </header>
+      <div class="container mx-auto p-4">
+        <slot />
+      </div>
     </div>
-</template>
-
-<style scoped>
-.router-link-exact-active {
-    color: #12b488
-}
-</style>
+  </template>
+  
+  <style scoped>
+    .router-link-exact-active {
+      color: #12b488;
+    }
+  </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,16 +1,23 @@
 <template>
     <div>
-        <p> Products </p>
-
+      <div class="grid grid-cols-4 gap-5">
+        <div v-for="p in products">
+          <ProductCard :product="p" />
+        </div>
+      </div>
     </div>
-</template>
-
-<script setup>
+  </template>
+  
+  <script setup>
+    //  fetch the products
+    const { data: products } = await useFetch('https://fakestoreapi.com/products')
     definePageMeta({
-        layout: 'products'
+      layout: "products",
     })
-</script>
-
-<style scoped>
-
-</style>
+    useHead({
+      title: 'Nuxt Dojo | Merch',
+      meta: [
+        { name: 'description', content: 'Nuxt 3 Merch'}
+      ]
+    })
+  </script>
